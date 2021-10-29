014bea7c6d709028d4bf3b0aca46d7718f7aeec4
Jaehun Park <pjaehun4122@gmail.com>
2021-10-29 14:02:44 -0700

    15passing get api actions complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,28 @@
-// Write your "actions" router here!
+const express=require('express')
+
+const Actions =require('./actions-model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next)=>{
+    Actions.get()
+    .then(response=>{
+        if(response){
+            res.status(200).json(response)}
+        else {
+           res.status(404).json(response) 
+        }
+    })
+    .catch(next)
+})
+
+
+
+router.use((err, req, res, next)=>{
+    res.status(err.status || 500).json({
+      customMessage: "something bad happend ",
+      message: err.message,
+      stack:err.stack,
+    })
+  })
+module.exports=router
