881fbc094cd5b7da30a19f3272d612bc1809364e
Jaehun Park <pjaehun4122@gmail.com>
2021-10-29 11:54:09 -0700

    starting project endporints

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const server = express();
 
+const projectRouter =require('./projects/projects-router')
+server.use(express.json())
+server.use('/api/projects', projectRouter )
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
-
+server.get('/', (req, res)=>{
+    res.send(`<h2>sprint challenge server test</h2>`)
+})
 module.exports = server;
--- index.js
+++ index.js
@@ -16,3 +16,6 @@ const server = require('./api/server')
 
 const PORT = process.env.PORT || 5000
 
+server.listen(PORT, ()=>{
+    console.log(`listening on ${PORT}`)
+})
