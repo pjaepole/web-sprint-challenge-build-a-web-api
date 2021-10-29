7e50066f66b69c7d3821efb2c34bc6c2edbcbefb
Jaehun Park <pjaehun4122@gmail.com>
2021-10-29 12:32:55 -0700

    get api/project/:id done, validate middleware created, general catch, 7passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,22 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+async function validateProjectID(req, res, next){
+    try{
+        const project= await Projects.get(req.params.id)
+        if(!project){
+            next({status:404, message: " project with that ID not found"})
+        } else {
+            req.project = project
+            next();
+        }
+    }
+    catch(err){
+        res.status(500).json({
+        message: 'problem finding project'
+      })}
+}
+
+module.exports ={
+    validateProjectID
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,15 +1,29 @@
 // Write your "projects" router here!
 const express = require('express')
-
+const {validateProjectID}=require('./projects-middleware')
 const Projects = require('./projects-model')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-    console.log('reached project router',res)
+router.get('/', (req,res,next)=>{
     Projects.get()
     .then(projects=>{
         res.status(200).json(projects)
     })
-    .catch(err=>{console.log(err)})
+    .catch(next)
 })
+router.get('/:id', validateProjectID, (req,res)=>{
+    res.status(200).json(req.project)
+})
+
+
+
+
+
+router.use((err, req, res, next)=>{
+    res.status(err.status || 500).json({
+      customMessage: "something bad happend ",
+      message: err.message,
+      stack:err.stack,
+    })
+  })
 module.exports=router
