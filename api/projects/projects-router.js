9cb73e5dad9f963edf9a473fd644e51399cb1a47
Jaehun Park <pjaehun4122@gmail.com>
2021-10-29 12:10:11 -0700

    get api project done 6passing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,5 +4,12 @@ const express = require('express')
 const Projects = require('./projects-model')
 const router = express.Router()
 
-
+router.get('/', (req,res)=>{
+    console.log('reached project router',res)
+    Projects.get()
+    .then(projects=>{
+        res.status(200).json(projects)
+    })
+    .catch(err=>{console.log(err)})
+})
 module.exports=router
