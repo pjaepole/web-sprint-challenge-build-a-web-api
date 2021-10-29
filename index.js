bfd423cdb75cb19dd6d815ad999d2a49500e5c49
Jaehun Park <pjaehun4122@gmail.com>
2021-10-29 11:32:33 -0700

    environment variable set

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 5000
+
